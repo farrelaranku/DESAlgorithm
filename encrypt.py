1a525c50c10f617cb63a476f110064ba1f15fb9b
farrelaranku <flinkin16@gmail.com>
2024-10-25 01:43:48 +0700

    first push, encrypt and decrypt

--- encrypt.py
+++ encrypt.py
@@ -1,4 +1,4 @@
-import random
+import socket
 
 print("Encryption")
 def permute(k, arr, n):
@@ -141,50 +141,66 @@ def encrypt(pt, rkb):
 	cipher_text = permute(combine, final_perm, 64)
 	return cipher_text
 
-key = ''.join(str(random.randint(0, 1)) for _ in range(64))
-print("Key: ", key)
+def start_server():
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	host = socket.gethostname()
+	port = 12345
+	server_socket.bind((host, port))
+	server_socket.listen()
 
-keyp = [57, 49, 41, 33, 25, 17, 9,
-		1, 58, 50, 42, 34, 26, 18,
-		10, 2, 59, 51, 43, 35, 27,
-		19, 11, 3, 60, 52, 44, 36,
-		63, 55, 47, 39, 31, 23, 15,
-		7, 62, 54, 46, 38, 30, 22,
-		14, 6, 61, 53, 45, 37, 29,
-		21, 13, 5, 28, 20, 12, 4]
+	print(f"Server berjalan di {host}:{port}")
 
+	client_socket, addr = server_socket.accept()
+	print(f"Menerima koneksi dari {addr}")
+	key = '1010101010101010101010101010101010101010101010101010101010101010'
 
-key = permute(key, keyp, 56)
-
-
-shift_table = [1, 1, 2, 2,
-			2, 2, 2, 2,
-			1, 2, 2, 2,
-			2, 2, 2, 1]
-
-
-key_comp = [14, 17, 11, 24, 1, 5,
-			3, 28, 15, 6, 21, 10,
-			23, 19, 12, 4, 26, 8,
-			16, 7, 27, 20, 13, 2,
-			41, 52, 31, 37, 47, 55,
-			30, 40, 51, 45, 33, 48,
-			44, 49, 39, 56, 34, 53,
-			46, 42, 50, 36, 29, 32]
+	keyp = [57, 49, 41, 33, 25, 17, 9,
+			1, 58, 50, 42, 34, 26, 18,
+			10, 2, 59, 51, 43, 35, 27,
+			19, 11, 3, 60, 52, 44, 36,
+			63, 55, 47, 39, 31, 23, 15,
+			7, 62, 54, 46, 38, 30, 22,
+			14, 6, 61, 53, 45, 37, 29,
+			21, 13, 5, 28, 20, 12, 4]
 
 
-left = key[0:28] 
-right = key[28:56]
-rkb = []
+	key = permute(key, keyp, 56)
 
-for i in range(0, 16):
-	
-	left = shift_left(left, shift_table[i])
-	right = shift_left(right, shift_table[i])
-	combine_str = left + right
-	round_key = permute(combine_str, key_comp, 48)
-	rkb.append(round_key)
-	
-pt = "ADA0123ADA456789"
-cipher_text = bin2hex(encrypt(pt, rkb))
-print("Cipher Text : ", cipher_text)
+
+	shift_table = [1, 1, 2, 2,
+				2, 2, 2, 2,
+				1, 2, 2, 2,
+				2, 2, 2, 1]
+
+
+	key_comp = [14, 17, 11, 24, 1, 5,
+				3, 28, 15, 6, 21, 10,
+				23, 19, 12, 4, 26, 8,
+				16, 7, 27, 20, 13, 2,
+				41, 52, 31, 37, 47, 55,
+				30, 40, 51, 45, 33, 48,
+				44, 49, 39, 56, 34, 53,
+				46, 42, 50, 36, 29, 32]
+
+
+	left = key[0:28] 
+	right = key[28:56]
+	rkb = []
+
+	for i in range(0, 16):
+		
+		left = shift_left(left, shift_table[i])
+		right = shift_left(right, shift_table[i])
+		combine_str = left + right
+		round_key = permute(combine_str, key_comp, 48)
+		rkb.append(round_key)
+
+		
+	pt = "ADA0123ADA456789"
+	ciphertext_h1 = bin2hex(encrypt(pt, rkb))
+	print("Plain Text : ", pt)
+	client_socket.send(ciphertext_h1.encode())
+	print(f"Chipertext yang dikirim ke client: {ciphertext_h1}")
+	client_socket.close()
+if __name__ == "__main__":
+    start_server()
